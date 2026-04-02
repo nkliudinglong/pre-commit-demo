f6f8aada082b1da810009190150deec7c778b578
liudinglong <liudinglong@alpha-star-research.com>
2026-04-02 15:15:32 +0800

    add test

---
+++ test/test_math.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# ############################################
+# Author：liudinglong
+# Filename：test_math.py
+# Created Time：2026-04-02
+# Description：
+# ############################################
+def test_addition():
+    assert 1 + 1 == 2
