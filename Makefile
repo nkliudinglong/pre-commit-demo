571ffbd819566d36e0a2b5cbcfb167d02a471e8a
liudinglong <liudinglong@alpha-star-research.com>
2026-04-02 15:45:33 +0800

    modify test

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 init:
-  pip install pre-commit
+	pip install pre-commit
 	pre-commit install --hook-type pre-push
+	@echo "✅ 环境配置完毕，Git Hook 已自动挂载！"
--- test/test_math.py
+++ test/test_math.py
@@ -1,10 +1,2 @@
-# -*- coding: utf-8 -*-
-
-# ############################################
-# Author：liudinglong
-# Filename：test_math.py
-# Created Time：2026-04-02
-# Description：
-# ############################################
 def test_addition():
-    assert 1 + 1 == 2
+    assert 1 + 1 == 3
